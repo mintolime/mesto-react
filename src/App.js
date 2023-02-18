823df8759b2485f062e312f684c39253b465c2f3
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-02-18 14:14:31 +0700

    feat:  realized PopupWithForm on App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ImagePopup from './components/ImagePopup';
 import Card from './components/Card';
 // import api from '../api/api'
 
-function App() {
+function App({ isEditProfilePopupOpen, isAddPlacePopupOpen, isEditAvatarPopupOpen }) {
 
   //   function handlePopupOpen() {
 
@@ -29,10 +29,38 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <PopupWithForm title="Редактировать профиль" name="edit-profile"  isEditProfilePopupOpen ={false} />
-      <PopupWithForm title="Новое место<" name="add-card"  isAddPlacePopupOpen = {false} />
-      <PopupWithForm title="Обновить аватар" name="avatar" isEditAvatarPopupOpen = {false} />
-      <PopupWithForm title="Вы уверены?" name="confirm" />
+      <PopupWithForm title="Редактировать профиль" name="edit-profile">
+        <fieldset className="form__inner">
+          <input className="form__input  form__input_text_name " name="nameUser" type="text" id="input-name"
+            aria-label="имя" placeholder="Ваше имя" minLength="2" maxLength="40" required />
+          <span className='form__input-error input-name-error'></span>
+          <input className="form__input form__input_text_about" name="aboutUser" type="text" id="input-about"
+            aria-label="подпись" placeholder="О себе" minLength="2" maxLength="200" required />
+          <span className='form__input-error input-about-error'></span>
+        </fieldset>
+      </PopupWithForm>
+
+      <PopupWithForm title="Новое место<" name="add-card"  >
+       <fieldset className="form__inner">
+              <input className="form__input  form__input_text_name" name="nameCard" type="text" id="input-name-card"
+                aria-label="Наименование" placeholder="Название" minLength="2" maxLength="30" required />
+              <span className='form__input-error input-name-card-error'></span>
+              <input className="form__input form__input_text_about" name="linkCard" type="url" id="input-link"
+                aria-label="подпись" placeholder="Ссылка на картинку" required />
+              <span className='form__input-error input-link-error'></span>
+            </fieldset>
+      </PopupWithForm>
+
+      <PopupWithForm title="Обновить аватар" name="avatar" >
+        <fieldset className="form__inner">
+          <input className="form__input" name="linkAvatar" type="url" id="input-avatar" aria-label="подпись" required />
+          <span className='form__input-error input-avatar-error'></span>
+        </fieldset>
+      </PopupWithForm>
+
+      <PopupWithForm title="Вы уверены?" name="confirm" >
+
+      </PopupWithForm>
       <ImagePopup />
       <Card />
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,75 +1,11 @@
-function PopupWithForm(props) {
+function PopupWithForm({ name, title, children }) {
   return (
     < >
-    {/* <section className="popup popup_edit-profile">
+      <section className={`popup popup_type_${name}`} aria-label="обновление аватара">
         <div className="popup__container">
-          <h3 className="popup__title">Редактировать профиль</h3>
-          <form name="formProfile" className="form" noValidate>
-            <fieldset className="form__inner">
-              <input className="form__input  form__input_text_name " name="nameUser" type="text" id="input-name"
-                aria-label="имя" placeholder="Ваше имя" minLength="2" maxLength="40" required />
-              <span className='form__input-error input-name-error'></span>
-              <input className="form__input form__input_text_about" name="aboutUser" type="text" id="input-about"
-                aria-label="подпись" placeholder="О себе" minLength="2" maxLength="200" required />
-              <span className='form__input-error input-about-error'></span>
-            </fieldset>
-            <button className="button button_type_save" type="submit">Сохранить</button>
-
-          </form>
-          <button type="button" className=" button button_type_close"></button>
-        </div>
-      </section>
-
-      <section className="popup popup_add-card" aria-label="добавление карточек">
-        <div className="popup__container">
-          <h3 className="popup__title">Новое место</h3>
-          <form name="formCard" className="form" noValidate>
-            <fieldset className="form__inner">
-              <input className="form__input  form__input_text_name" name="nameCard" type="text" id="input-name-card"
-                aria-label="Наименование" placeholder="Название" minLength="2" maxLength="30" required />
-              <span className='form__input-error input-name-card-error'></span>
-              <input className="form__input form__input_text_about" name="linkCard" type="url" id="input-link"
-                aria-label="подпись" placeholder="Ссылка на картинку" required />
-              <span className='form__input-error input-link-error'></span>
-            </fieldset>
-            <button className="button button_type_save" type="submit">Создать</button>
-          </form>
-          <button type="button" className=" button button_type_close"></button>
-        </div>
-      </section>
-
-      <section className="popup popup_confirm" aria-label="подтверждение удаления">
-        <div className="popup__container">
-          <form name="formConfirm" className="form" noValidate>
-            <h3 className="popup__title">Вы уверены?</h3>
-            <button className="button button_type_save button_type_save-confirm" type="submit">Да</button>
-          </form>
-          <button type="button" className=" button button_type_close"></button>
-        </div>
-      </section>
-
-      <section className="popup popup_avatar" aria-label="обновление аватара">
-        <div className="popup__container">
-          <h3 className="popup__title">Обновить аватар</h3>
-          <form name="formAvatar" className="form" noValidate>
-            <fieldset className="form__inner">
-              <input className="form__input" name="linkAvatar" type="url" id="input-avatar" aria-label="подпись" required />
-              <span className='form__input-error input-avatar-error'></span>
-            </fieldset>
-            <button className="button button_type_save button_type_save-avatar" type="submit">Сохранить</button>
-          </form>
-          <button type="button" className=" button button_type_close"></button>
-        </div>
-      </section> */}
-
-            <section className={`popup popup_type_${props.name}`}   aria-label="обновление аватара">
-        <div className="popup__container">
-          <h3 className="popup__title">${props.title}`</h3>
-          <form name={`${props.name}`}  className="form" noValidate>
-            <fieldset className="form__inner">
-              <input className="form__input" name="linkAvatar" type="url" id="input-avatar" aria-label="подпись" required />
-              <span className='form__input-error input-avatar-error'></span>
-            </fieldset>
+          <h3 className="popup__title">{title}`</h3>
+          <form name={name} className="form" noValidate>
+             {children}
             <button className="button button_type_save button_type_save-avatar" type="submit">Сохранить</button>
           </form>
           <button type="button" className=" button button_type_close"></button>
@@ -77,7 +13,7 @@ function PopupWithForm(props) {
       </section>
 
     </>
-    )
+  )
 }
 
 
