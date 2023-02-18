306afc9e82956b1653ad568091f9b40dea550537
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-02-18 15:51:51 +0700

    dev: realised open all Popup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,22 @@
-function Main() {
 
+function Main({onEditAvatar,onEditProfile,onAddPlace}) {
+   function handleOpenClick() {
+   const popup = document.querySelector('.popup_type_{name}')
+    popup.classList.add('popup_opened')
+  }
 
   return (
       <main className="main root__container">
 
         <section className="profile">
           <img className="profile__photo" src="#" alt="фото профиля" />
-          <button className="button button_type_avatar-edit" onEditAvatar={false}></button>
+          <button className="button button_type_avatar-edit" onClick={handleOpenClick}></button>
           <div className="profile__content">
             <h1 className="profile__name">Жак кустарник</h1>
-            <button className="button button_type_edit" onEditProfile={false}></button>
+            <button className="button button_type_edit" onClick={handleOpenClick}></button>
             <p className="profile__info">Редкий вид</p>
           </div>
-          <button className="button button_type_add" type="button" onAddPlace={false}></button>
+          <button className="button button_type_add" type="button" onClick={handleOpenClick}></button>
         </section>
 
         <section className="cards" aria-label="галерея пользователя">
