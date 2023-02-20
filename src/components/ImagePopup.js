087b25414b4d367521b89f74a8eaf4d143a438ee
mintolime <104442896+mintolime@users.noreply.github.com>
2023-02-20 10:46:34 +0000

    fix: added alt on images

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,7 +4,7 @@ function ImagePopup({card,onClose,isOpen}) {
       <section className={`popup popup_image  ${isOpen ? 'popup_opened' : ''}`} aria-label="фотографии карточек">
         <div className="popup__container popup__container_image ">
           <figure className="figure">
-            <img className="figure__image" src={card.link} alt="test" />
+            <img className="figure__image" src={card.link} alt={card.name} />
             <figcaption className="figure__info">{card.name}</figcaption>
           </figure>
           <button type="button" className=" button button_type_close" onClick={onClose}></button>
